b0aab555eb637b5d4a089b74d11db1ae7719a285
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2021-08-30 02:10:12 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace NulityCrash {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(10, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(317, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CrashNulity";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(10, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Unity.exe ?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(10, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nb ?";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(10, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Physical memory usage";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(10, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Base priority";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(10, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Priority class";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(10, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "User processor time";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Location = new System.Drawing.Point(10, 206);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(124, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Privileged processor time";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label8.Location = new System.Drawing.Point(10, 229);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(102, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Total processor time";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label9.Location = new System.Drawing.Point(10, 250);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(133, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Paged system memory size";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label10.Location = new System.Drawing.Point(10, 272);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(98, 13);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Paged memory size";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label11.Location = new System.Drawing.Point(154, 365);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 13);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "label11";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(338, 321);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Nulity Crasher";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NulityCrash {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            TopMost = true;
+            var processes = Process.GetProcessesByName("unity");
+            foreach(var p in processes) {
+                KillAllProcessesSpawnedBy((UInt32)p.Id);
+            }
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if(Process.GetProcessesByName("unity").Length == 0) {
+                label1.Text = "No Nulity Process Open";
+                return;
+            } else {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C taskkill /IM unity.exe /F & unity.exe";
+                process.StartInfo = startInfo;
+                startInfo.CreateNoWindow = true;
+                process.Start();
+                label1.Text = "No Nulity Process Open";
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void KillAllProcessesSpawnedBy(UInt32 parentProcessId) {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(
+                "SELECT * " +
+                "FROM Win32_Process " +
+                "WHERE ParentProcessId=" + parentProcessId);
+            ManagementObjectCollection collection = searcher.Get();
+            if(collection.Count > 0) {
+                foreach(var item in collection) {
+                    UInt32 childProcessId = (UInt32)item["ProcessId"];
+                    if((UInt32)childProcessId != Process.GetCurrentProcess().Id) {
+                        KillAllProcessesSpawnedBy(childProcessId);
+                        Process childProcess = Process.GetProcessById((int)childProcessId);
+                        label2.Text = childProcessId.ToString();
+                        childProcess.Kill();
+                    }
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            var processes = Process.GetProcessesByName("unity");
+            foreach(var p in processes) {
+                try {
+                    label3.Text = $"Physical memory usage     : {p.WorkingSet64 / (1024 * 1024)} Mb";
+                    label4.Text = $"Base priority             : {p.BasePriority}";
+                    label5.Text = $"Priority class            : {p.PriorityClass}";
+                    label6.Text = $"User processor time       : {p.UserProcessorTime.Hours} : {p.UserProcessorTime.Minutes} : {p.UserProcessorTime.Seconds}";
+                    label7.Text = $"Privileged processor time : {p.PrivilegedProcessorTime.Hours} : {p.PrivilegedProcessorTime.Minutes} : {p.PrivilegedProcessorTime.Seconds}";
+                    label8.Text = $"Total processor time      : {p.TotalProcessorTime.Hours} : {p.TotalProcessorTime.Minutes} : {p.TotalProcessorTime.Seconds}";
+                    label9.Text = $"Paged system memory size  : {p.PagedSystemMemorySize64 / (1024 * 1024)} Mb";
+                    label10.Text = $"Paged memory size         : {p.PagedMemorySize64 / (1024 * 1024)} Mb";
+                    if(p.Responding) {
+                        button1.BackColor = Color.Black;
+                    } else {
+                        button1.BackColor = Color.Red;
+                    }
+                }
+                catch(Exception f) { }
+            }
+            if(processes.Length != 0) {
+                button1.Enabled = true;
+                label1.Text = "Unity Found!";
+            } else {
+                button1.Enabled = false;
+            }
+        }
+
+        public bool IsProcessResponding(Process process) {
+            if(process.MainWindowHandle == IntPtr.Zero) {
+                return (true);
+            } else {
+                if(!process.Responding) {
+                    Console.WriteLine("Process " + process.ProcessName +
+                      " is not responding.");
+                    return (false);
+                } else {
+                    Console.WriteLine("Process " + process.ProcessName +
+                      " is responding.");
+                    return (true);
+                }
+            }
+        }
+    }
+}
